b5b912e989d4d6bffd0a3b407be92c1dd9d7481f
Evan Furbeyre <efurbeyre@redzone.co>
2018-11-04 14:17:00 -0700

    restructured files before redux addition

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Subcell from './Subcell'
 
-
-export default function Cell({subcells, value, display}) {
-  return (
-    <div className='cell'>
-      <div className='subcells'>
-        <Subcell display={display}/>
-        <Subcell display={display}/>
+export default class Cell extends Component {
+  render() {
+    return (
+      <div className='cell'>
+        <div className='subcells'>
+          <Subcell display={this.props.display}/>
+          <Subcell display={this.props.display}/>
+        </div>
+        {this.props.display ? this.props.value : ''}    
       </div>
-      {display ? value : ''}    
-    </div>
-  )
+    )
+  }
 }
